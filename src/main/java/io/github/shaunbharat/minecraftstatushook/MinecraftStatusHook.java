7c3fcf5fc681b3359ae37de50e2e27f3ae830f68
Shaun Bharat <79479981+shaunbharat@users.noreply.github.com>
2024-07-02 22:19:02 -0400

    feat: add togglerefreshpotd command and explicitly setting potd (#3)

--- src/main/java/io/github/shaunbharat/minecraftstatushook/MinecraftStatusHook.java
+++ src/main/java/io/github/shaunbharat/minecraftstatushook/MinecraftStatusHook.java
@@ -2,6 +2,7 @@ package io.github.shaunbharat.minecraftstatushook;
 
 import io.github.shaunbharat.minecraftstatushook.commands.Potd;
 import io.github.shaunbharat.minecraftstatushook.commands.SetDiscordWebhook;
+import io.github.shaunbharat.minecraftstatushook.commands.ToggleRefreshPotd;
 import io.github.shaunbharat.minecraftstatushook.listeners.PlayerListener;
 import io.github.shaunbharat.minecraftstatushook.utils.Color;
 import io.github.shaunbharat.minecraftstatushook.utils.Webhook;
@@ -30,6 +31,7 @@ public final class MinecraftStatusHook extends JavaPlugin {
         // Register commands
         new SetDiscordWebhook(this);
         new Potd(this);
+        new ToggleRefreshPotd(this);
 
         webhook.sendEmbed("Minecraft Server Status", "Server has been started", Color.MEDIUM_SEA_GREEN);
     }
--- src/main/java/io/github/shaunbharat/minecraftstatushook/commands/ToggleRefreshPotd.java
+++ src/main/java/io/github/shaunbharat/minecraftstatushook/commands/ToggleRefreshPotd.java
@@ -1,30 +1,22 @@
 package io.github.shaunbharat.minecraftstatushook.commands;
 
 import io.github.shaunbharat.minecraftstatushook.MinecraftStatusHook;
-import io.github.shaunbharat.minecraftstatushook.utils.Webhook;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-public class Potd implements CommandExecutor {
-    private final Webhook webhook = MinecraftStatusHook.webhook;
+
+public class ToggleRefreshPotd implements CommandExecutor {
     private final io.github.shaunbharat.minecraftstatushook.utils.Potd potd = MinecraftStatusHook.potd;
 
-    public Potd(MinecraftStatusHook plugin) {
-        plugin.getCommand("potd").setExecutor(this);
+    public ToggleRefreshPotd(MinecraftStatusHook plugin) {
+        plugin.getCommand("togglerefreshpotd").setExecutor(this);
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        String newPotd;
-        if (args.length == 1) {
-            newPotd = args[0];
-            potd.setPotd(args[0]);
-        } else {
-            newPotd = potd.generatePotd();
-        }
-        sender.sendMessage("Refreshed password of the day: \"" + newPotd + "\"");
+        potd.setRefreshPotd(!potd.getRefreshPotd());
         return true;
     }
 }
